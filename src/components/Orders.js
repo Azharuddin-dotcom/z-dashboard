85e6895c204673d14de52192fa31076111ce7210
Mr. Azharuddin <azharquraishi94@gmail.com>
2025-03-16 00:36:16 +0530

    ok

---
+++ src/components/Orders.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import { Link } from "react-router-dom";
+
+const Orders = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/allOrders").then((res) => {
+      setOrders(res.data);
+    });
+  }, []);
+
+  if (orders.length === 0) {
+    return (
+      <div className="no-orders">
+        <p>You haven't placed any orders today</p>
+
+        <Link to={"/"} className="btn">
+          Get started
+        </Link>
+      </div>
+    );
+  } else {
+    return (
+      <div className="orders">
+        <h1>Order List</h1>
+        <div>
+          {orders.map((order) => (
+            <div
+              key={order._id}
+              style={{
+                border: "1px solid #ddd",
+                padding: "10px",
+                marginBottom: "10px",
+              }}
+            >
+              <h2>{order.name}</h2>
+              <p>
+                <strong>Quantity:</strong> ${order.qty}
+              </p>
+              <p>{order.price}</p>
+              <p>{order.mode}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Orders;
